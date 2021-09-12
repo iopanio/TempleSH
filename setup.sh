9137d29846d2930d461444af4d466bddf1e98fd6
svlieri <oriondo.abp@gmail.com>
2021-09-12 21:16:22 +0800

    Add setup file

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+chmod +x temple
+sudo cp temple /usr/local/bin/
+
+cat .Xresources >> ~/.Xresources
+xrdb -merge .Xresources
+cat .xinitrc >> ~/.xinitrc
+
+chmod +x key-handler
+cp key-handler ~/.config/sxiv/exec/
