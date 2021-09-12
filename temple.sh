ffb39fa0eb47b201d6208be95def2d6e3a852aa2
svlieri <oriondo.abp@gmail.com>
2021-09-12 17:47:56 +0800

    Pipe mp4-png

--- temple.sh
+++ temple.sh
@@ -2,13 +2,12 @@
 
 dir="$*"
 if [ -z "$*" ]; then dir="$PWD"; fi
-for f in *mov *mp4 *mkv *webm
+for f in $dir/*mp4
 do
-	if [ ! -f "$dir/$f.png" ] || [ ! -f "$dir/$f.jpg" ]
-	then  
-            sstime=$(ffmpeg -y -i "$f" 2>&1 |\
-			 awk '/Duration/ { split($2, A, ":"); print (3600*A[1] + 60*A[2] + A[3]) /5 }')
-	    ffmpeg -v quiet -y -ss $sstime -i "$f" -vframes 1 "$(echo $f | sed 's/mp4/png/')"
+	if [ -f "$(echo $f | sed 's/.mp4/.jpg/')" ]; then continue; fi
+	if [ ! -f "$(echo $f | sed 's/.mp4/.png/')" ]; then
+            sstime=$(ffmpeg -y -i "$f" 2>&1 | awk '/Duration/ { split($2, A, ":"); print (3600*A[1] + 60*A[2] + A[3]) /5 }')
+	    ffmpeg -v quiet -y -ss $sstime -i "$f" -vframes 1 "$(echo $f | sed 's/.mp4/.png/')"
 	fi
 done
 sxiv $dir
